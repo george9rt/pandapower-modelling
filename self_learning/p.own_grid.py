d8658a0f0a9093aaec5d44ae747670e12825c6b8
george9rt <george.tolto@outlook.com>
2025-04-12 16:57:59 -0500

    changes

--- self_learning/p.own_grid.py
+++ self_learning/p.own_grid.py
@@ -15,10 +15,6 @@ b1 = pp.create_bus(net, vn_kv=20., name="Bus 1")
 b2 = pp.create_bus(net, vn_kv=0.4, name="Bus 2")
 b3 = pp.create_bus(net, vn_kv=0.4, name="Bus 3")    
 
-#Load
-    #Active Power: 100kW
-    #Reactive Power: 50 kVar
-
 #create elements 
 pp.create_ext_grid(net, bus=b1, vm_pu=1.02, name="Grid Connection")
 pp.create_load(net, bus =b3, p_mw=0.1, q_mvar=0.05, name="Load")
